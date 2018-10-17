79349503a90bae6118b5bc0bfdb6523f24949213
arshad <arshadalam23@gmail.com>
2018-10-17 23:47:15 +0530

    modified code in TestA Class

--- src/array/TestA.java
+++ src/array/TestA.java
@@ -5,6 +5,8 @@ public class TestA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test A for git demo only");
+		System.out.println("for branch understanding ");
+		
 
 	}
 
